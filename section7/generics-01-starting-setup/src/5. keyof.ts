ebf7732b4e9e021066787d522c6e17803ad5e63c
starplanter93 <starplanter93@gmail.com>
2023-01-03 16:23:58 +0900

    keyof

--- section7/generics-01-starting-setup/src/5. keyof.ts
+++ section7/generics-01-starting-setup/src/5. keyof.ts
@@ -1,4 +1,4 @@
-function merge<T extends object, U extends object>(objA: T, objB: U) {
+/* function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
@@ -35,3 +35,4 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: "jks" }, "name");
+ */
