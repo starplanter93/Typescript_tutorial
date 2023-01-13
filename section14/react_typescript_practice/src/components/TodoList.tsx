0044e1001205189f0047b3ff3aa5a1caa948a2b2
starplanter93 <starplanter93@gmail.com>
2023-01-13 11:04:47 +0900

    props 전달하기

---
+++ section14/react_typescript_practice/src/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface TodoListProps {
+  items: { id: string; text: string }[];
+}
+
+function TodoList({ items }: TodoListProps) {
+  return (
+    <ul>
+      {items.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
