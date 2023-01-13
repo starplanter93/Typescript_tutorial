209cb6c16a108b0c89108dd0495fac12bf8a3bcb
starplanter93 <starplanter93@gmail.com>
2023-01-13 11:53:57 +0900

    삭제버튼 및 css 추가

--- section14/react_typescript_practice/src/components/NewTodo.tsx
+++ section14/react_typescript_practice/src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import "./NewTodo.css";
 
 type NewTodoProps = {
   onAddTodo: (todoText: string) => void;
--- section14/react_typescript_practice/src/components/TodoList.tsx
+++ section14/react_typescript_practice/src/components/TodoList.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import "./TodoList.css";
 
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
-function TodoList({ items }: TodoListProps) {
+function TodoList({ items, onDeleteTodo }: TodoListProps) {
   return (
     <ul>
       {items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={onDeleteTodo.bind(null, todo.id)}>DELETE</button>
+        </li>
       ))}
     </ul>
   );
